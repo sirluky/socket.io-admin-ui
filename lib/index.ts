a0df755f1c856fc3d4afc0dd02a72286e93a9b97
Lukáš Kovář <38957065+sirluky@users.noreply.github.com>
2024-01-15 14:51:59 +0100

    fix: prevent crash when admin do not exists

--- lib/index.ts
+++ lib/index.ts
@@ -357,12 +357,14 @@ const registerVerboseListeners = (
     );
 
     socket.conn.on("upgrade", (transport: any) => {
-      socket.data._admin.transport = transport.name;
-      adminNamespace.emit("socket_updated", {
-        id: socket.id,
-        nsp: nsp.name,
-        transport: transport.name,
-      });
+      if (socket.data?._admin) {
+        socket.data._admin.transport = transport.name;
+        adminNamespace.emit("socket_updated", {
+          id: socket.id,
+          nsp: nsp.name,
+          transport: transport.name,
+        });
+      }
     });
 
     if (nsp !== adminNamespace) {
